0b76f103ea751e0e591f83f6746b4a9b40e95e2e
Harish-Kushwah <harishkushwah54321@gmail.com>
2026-01-04 23:56:02 +0530

    asg: assigment 3 question 1 added

---
+++ assignments/asg_3/copy_file.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<string.h>
+#include<unistd.h>
+
+int main(int argc , char* argv[])
+{   
+    int source_fd = 0 , dest_fd = 0;
+    char buff[4096];
+    memset(buff , '\0' , sizeof(buff));
+
+    if(argc < 3)
+    {
+        printf("Error: Enter both file names");
+        return 0;
+    }
+    source_fd = open(argv[1] , O_RDONLY);
+
+    if(source_fd < 0)
+    {
+        printf("Error in soruce file : %s\n", strerror(errno));
+        return -1;
+    }
+
+    dest_fd = open(argv[2] , O_WRONLY | O_CREAT | O_TRUNC , 0777);
+
+    if(dest_fd < 0)
+    {
+        printf("Error in destination file : %s\n", strerror(errno));
+        return -1;
+    }
+
+    while (read(source_fd , &buff , sizeof(buff)))
+    {
+        write(dest_fd,&buff , sizeof(buff));
+        memset(buff , '\0' , sizeof(buff));
+    }
+
+    printf("file coppied successfully from %s to %s\n" , argv[1] , argv[2]);
+
+    close(source_fd);
+    close(dest_fd);
+    
+    return 0;
+}
